390c55a47a732f5d8b5bda8f1fab2c9ae97cea78
Zeeland <zeeland@foxmail.com>
2023-01-17 02:19:44 +0800

    test: add some unit tests

---
+++ tests/test_adapter.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from main import *
+from image_service import get_new_url, _download_img
+import yaml_service
+
+
+class TestAdapter(TestCase):
+
+    def test_aliyun_upload(self):
+        pass
+
+    def test_aliyun_get_url(self):
+        pass
